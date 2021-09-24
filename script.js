fedd17d737d6043364758a3fd7cde18fd5b0aadd
Ravneet Panglia <ravneet.panglia@gmail.com>
2021-09-24 00:19:33 -0400

    produces random password meeting confirmed length

--- script.js
+++ script.js
@@ -5,30 +5,19 @@
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "+", "=", "~", "?", "/", ",", "|", "{", "}", "[", "]", "<", ">", ",", ".", ":", ";", "`"]; 
+var special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "+", "=", "~", "?", "/", ",", "|", "{", "}", "[", "]", "<", ">", ".", ":", ";", "`"]; 
 var confirmLength = "";
 var confirmUpperCase;
 var confirmLowerCase;
 var confirmNumber;
 var confirmSpecial;
+var password = "";
+var avalChars = "";
 
 
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
 
 // Once user clicks on "Generate Password" button, series of prompts for password criteria appear
 //1. confirm length, 2. uppercase&confirm, 3. lowercase &confirm, 4. number, 5. special characters
@@ -63,30 +52,76 @@ function generatePassword() {
         //how to loop lines 59-62 and clean up code????  also where to add console log for the above 4 lines??
     }
 
-    var userPassword = [];
 
-    // use .concat to merge 2 or more arrays
+    // can use .concat to merge 2 or more arrays
     
-    if(confirmUpperCase) {
-        userPassword = userPassword.concat(upperCase);
+    if (confirmUpperCase) {
+        avalChars += upperCase;
     }
 
     if(confirmLowerCase) {
-        userPassword = userPassword.concat(lowerCase);
+        avalChars += lowerCase;
     }
 
     if(confirmNumber) {
-        userPassword = userPassword.concat(number);
+        avalChars += number;
     }
 
     if(confirmSpecial) {
-        userPassword = userPassword.concat(special);
+        avalChars += special;
     }
+    
 
-    console.log(userPassword)
+
+    for (var i = 0; i < confirmLength; i++) {
+        //randomly select chars from chars added to availchars. Loop thru selecting of chars until matches confirm.length
+        password += avalChars[Math.floor(Math.random() * avalChars.length)];
+    }
+        
+  
+    return password;
+    
+}
+// **********QUESTION===HOW TO GET FIELD TO CLEAR BY CLICKING ON GENERATE PASSWORD BUTTON AGAIN!???
+
+
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
 
 }
 
-var finalPassword = [];
 
+/* in function() =  generate password
+    UNDER IN(CONFIRM all 4 === false)
+    IN FOR LOOP:
+    //randomly select chars from chars added to availchars. Loop thru selecting of chars until matches confirm.length
+        password += avalChars[Math.floor(Math.random() * avalChars.length)];
+        //var randomNumber = userInput[Math.floor(Math.random() * userInput.confirmLength)];
+        //randomPassword.push(randomNumber);
+        //password = password + userInput[Math.floor(Math.random() * userInput.confirmLength)];
+        //console.log(randomNumber);
 
+    AFTER RETURN func:
+    console.log(userInput)
+
+    var password = "";
+
+    for (var i = 0; i <= confirmLength; i++) {
+        var randomNumber = Math.floor(Math.random() * userInput.confirmLength);
+        CUT---password = password + userInput[Math.floor(Math.random() * userInput.confirmLength)];
+        console.log(password);
+        return password;
+    
+    var finalpassword = password.join("");
+    console.log(password);
+    window.alert("Your Random Password is: " + finalpassword);
+    return finalPassword;*/
